686d977ea8a2cd93343fd72cbee1c3683d53c667
giacomo1989 <voltolina.giacomo89@gmail.com>
2018-01-26 21:52:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def makeWebhookResult(data):
 
     
     speech = item["forecast"][0].get("day")+ ", " + item["forecast"][0].get("date") + "\n\nToday in " + location.get('city') + ": " + condition.get('text') + \
-             " and the temperature is "+ condition.get('temp') + " " + units.get('temperature')
+             " and the temperature is "+ str(int((int(condition.get('temp'))-32)/1.8) + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
