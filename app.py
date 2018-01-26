179e4bc612f8068cff15c336590c72b297e52146
giacomo1989 <voltolina.giacomo89@gmail.com>
2018-01-26 15:07:55 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+    speech = "prova prova Today the weather in " + location.get('city') + ": " + condition.get('text') + \
              ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
