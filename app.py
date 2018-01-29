00d23d62b10575314c6782cda9bd66bf7ac911d2
giacomo1989 <voltolina.giacomo89@gmail.com>
2018-01-29 17:13:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -100,8 +100,8 @@ def makeWebhookResult(data):
     # print(json.dumps(item, indent=4))
 
     celsius=int(condition.get('temp'))
-    Gc=((celsius-32)/1.8)
-    speech = item["forecast"][0].get("day")+ ", guillelme" + item["forecast"][0].get("date") + "\n\nToday in " + location.get('city') + ": " + condition.get('text') + \
+    Gc=int((celsius-32)/1.8)
+    speech = item["forecast"][0].get("day")+ ", " + item["forecast"][0].get("date") + "\n\nToday in " + location.get('city') + ": " + condition.get('text') + \
              " and the temperature is "+ str(Gc) + "  " + units.get('temperature') + ", with the humidity of " + atmosphere.get('humidity') +"%"
 
     print("Response:")
