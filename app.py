3ab73dfa8746382435ff56f699e0ba78b87c46ec
giacomo1989 <voltolina.giacomo89@gmail.com>
2018-01-29 17:20:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def makeWebhookResult(data):
 
     grados=int(condition.get('temp'))
     Gc=int((grados-32)/1.8)
-    speech = item["forecast"][0].get("day")+ ", " + item["forecast"][0].get("date") + "\n\nToday in " + location.get('city') + ": " + condition.get('text') + \
+    speech = item["forecast"][0].get("day")+ ", alhora" + item["forecast"][0].get("date") + "\n\nToday in " + location.get('city') + ": " + condition.get('text') + \
              " and the temperature is "+ str(Gc) + "  " + units.get('temperature') + ", with the humidity of " + atmosphere.get('humidity') +"%"
 
     print("Response:")
